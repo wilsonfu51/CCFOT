267fca49c52c72ea128e2ceec1cc50a6f34b347a
wilsonfu51 <f524258467@yeah.net>
2016-06-30 11:39:08 +0800

    Add files via upload
    add sql files

---
+++ development/T_CCFOT_FAFB.sql
@@ -0,0 +1,28 @@
+USE [odw_staging]
+GO
+
+/****** Object:  Table [odw_user].[T_CCFOT_FAFB]    Script Date: 2016/6/30 11:37:35 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [odw_user].[T_CCFOT_FAFB](
+	[SKU_CATEGORY] [nvarchar](200) NOT NULL,
+	[BU] [nvarchar](20) NOT NULL,
+	[SKU_DES] [nvarchar](2000) NULL,
+	[MATERIAL_TYPE] [nvarchar](20) NULL,
+	[STANDARD_CODE] [nvarchar](200) NULL,
+	[SKU_NUM] [int] NULL,
+	[MONTH_FA] [decimal](15, 3) NULL,
+	[MONTH_FB] [decimal](15, 3) NULL,
+	[YTD_FA] [decimal](15, 3) NULL,
+	[YTD_FB] [decimal](15, 3) NULL,
+	[LAST_UPD_DT] [datetime] NULL,
+	[JOB_ID] [nvarchar](10) NULL
+) ON [PRIMARY]
+
+GO
+
+
